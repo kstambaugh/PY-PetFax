4bfc59a4abd02b0c14217414a4d8c0e4df1ebed5
kstambaugh <kyle.w.stambaugh1@gmail.com>
2022-11-06 14:05:23 -0500

    finished 3rd inclass activity

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -2,32 +2,27 @@ from flask import Flask
 from flask_migrate import Migrate
 
 
+# factory
 def create_app():
     app = Flask(__name__)
 
-# database config
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/petfax'
     app.config['SQLALCHEMY_TRACK_MODIFICATION'] =False
-
-   
 
     from . import models
     models.db.init_app(app)
     migrate = Migrate(app, models.db)
     
-    
-
-    
-
+    #index route
     @app.route('/')
     def index():
         return 'Hello, this is PetFax!'
 
-#register pet blueprint
+    #register pet blueprint
     from . import pet
     app.register_blueprint(pet.pet_bp)
 
-# register fact blueprint
+    # register fact blueprint
     from . import fact
     app.register_blueprint(fact.fact_bp)
 
--- petfax/fact.py
+++ petfax/fact.py
@@ -1,14 +1,22 @@
-from flask import ( Blueprint, render_template, request, redirect ) 
+from flask import ( Blueprint, render_template, request, redirect )
+from . import models 
 
 fact_bp = Blueprint('fact', __name__, url_prefix="/facts")
 
 @fact_bp.route('/', methods=['GET', 'POST'])
 def index(): 
     if request.method == 'POST':
+        submitter = request.form['submitter']
+        fact = request.form['fact']
+
+        new_fact = models.Fact(submitter=submitter,fact=fact)
+        models.db.session.add(new_fact)
+        models.db.session.commit()
         print(request.form)
         return redirect('/facts')
     
-    return render_template('facts/index.html')
+    results = models.Fact.query.all()
+    return render_template('facts/index.html', facts = results)
 
 @fact_bp.route('/new')
 def new(): 
